41f8f47557c9825279d46f755119732a2c15ce03
Ayush Arya <ayusharyaft63@gmail.com>
2026-03-26 20:46:53 +0530

    Create DishaPortalApplication main class

---
+++ src/main/java/com/example/tulip/DishaPortalApplication.java
@@ -0,0 +1,12 @@
+package com.example.tulip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishaPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishaPortalApplication.class, args);
+    }
+}
